c1b38a873191628be90448f28d09119bb5f5ad58
NotAUserName0 <dill1@live.com.mx>
2022-11-22 13:51:57 -0600

    add

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
 $ch = curl_init();
 
+$headers = [
+    "Authorization: Client-ID 7r06Y5mNPA32P0p-jvRIXr7dKhs3U"
+];
+
 //curl_setopt($ch, CURLOPT_URL,"https://randomuser.me/api");
 //curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 
 curl_setopt_array($ch,[
-    CURLOPT_URL => "https://randomuser.me/api",
-    CURLOPT_RETURNTRANSFER => true
+    CURLOPT_URL => "https://api.unsplash.com/photos/random",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HEADER => $headers
 ]);
 
 $response = curl_exec($ch);
+
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
 curl_close($ch);
+
+echo $status_code, "\n";
 
 echo $response, "\n";

a4fd60a343948cb08c3c6cdf96e26d1a62e09dfb
NotAUserName0 <dill1@live.com.mx>
2022-11-22 13:24:47 -0600

    add

--- index.php
+++ index.php
@@ -1,35 +1,15 @@
 <?php
+$ch = curl_init();
 
-if(!empty($_GET["name"])){
-    $response = file_get_contents("https://api.agify.io?name={$_GET["name"]}");
-    $data = json_decode( $response, true );
+//curl_setopt($ch, CURLOPT_URL,"https://randomuser.me/api");
+//curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 
-    $age = $data["age"];
-}
+curl_setopt_array($ch,[
+    CURLOPT_URL => "https://randomuser.me/api",
+    CURLOPT_RETURNTRANSFER => true
+]);
 
-//var_dump($data);
+$response = curl_exec($ch);
+curl_close($ch);
 
-//echo $data["results"][0]["name"]["first"], "\n";
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php if(isset($age)): ?>
-        Age : <?= $age ?>
-    <?php endif; ?>    
-
-    <form action="">
-        <label for="name">Name</label>
-        <input name="name" id="name" type="text">
-        <button type="submit">Guess age</button>
-    </form>
-</body>
-</html>
+echo $response, "\n";
